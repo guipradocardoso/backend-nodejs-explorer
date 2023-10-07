22c5f00232ea4bf6e1e7e7ea07ba243a7581dd31
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-10-07 15:55:06 -0300

    body params

--- src/server.js
+++ src/server.js
@@ -1,23 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-   const { id, user } = request.params;
+app.post("/users", (request, response) => {
+   const { name, email, password} = request.body;
 
-   response.send(`
-   Mensagem ID: ${id}.
-   Para o usuário: ${user}.
-   `);
-});
-
-app.get("/users", (request, response) => {
-   const { page, limit } = request.query;
-
-   response.send(`
-   Página: ${page}.
-   Limite: ${limit}.
-   `);
+   response.json({ name, email, password});
 
 })
 
