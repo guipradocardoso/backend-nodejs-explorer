2d41e0ba5242e50b45aa10021175f0d0e55473be
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-10-07 16:06:55 -0300

    organizing project structure

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
+const routes = require("./routes");
+
+
 const app = express();
 app.use(express.json());
+
+app.use(routes);
 
 app.post("/users", (request, response) => {
    const { name, email, password} = request.body;
