2ca72201db9bfe2af47167e29d077ca59ba8acba
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-10-07 16:14:56 -0300

    controller

---
+++ src/controllers/UserController.js
@@ -0,0 +1,20 @@
+class UserController {
+   /**
+      * index - GET para listar vários registros.
+      * show - GET para exibir um registro específico.
+      * create - POST para criar um registro.
+      * update - PUT para atualizar um registro.
+      * delete - DELETE para remover um registro
+   */
+  
+
+
+
+}
+
+
+
+
+
+
+module.exports = UserController;
