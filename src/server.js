48038d8489e9b73428158cade94a45241d45a700
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-10-08 19:40:55 -0300

    exception handling

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
 const express = require('express');
 
 const routes = require("./routes");
@@ -14,6 +17,25 @@ app.post("/users", (request, response) => {
    response.json({ name, email, password});
 
 })
+
+
+app.use((error, request, response, next) => {
+   if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+         status: "error",
+         message: error.message
+      });
+   }
+
+   console.error(error);
+
+
+   return response.status(500).json({
+      status: "error",
+      message: "Internal server error"
+
+   });
+});
 
 
 const PORT = 3333;
