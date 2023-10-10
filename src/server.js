b7d385f60a45d091be37e3d9797c855d2ed8fa10
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-10-09 21:18:18 -0300

    connecting with database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require("express-async-errors")
 
+const database = require("./database/sqlite")
+
 const AppError = require("./utils/AppError")
 const express = require('express');
 
@@ -9,7 +11,11 @@ const routes = require("./routes");
 const app = express();
 app.use(express.json());
 
+
+
 app.use(routes);
+
+database();
 
 app.post("/users", (request, response) => {
    const { name, email, password} = request.body;
